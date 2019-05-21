fa9664073cb225b5957df6a8777579f22e7ff577
Christopher Foley <chrisninetyone@gmail.com>
2019-05-21 15:03:20 +0800

    create CRUD actions of task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'index', to: 'tasks#index', as: :index
+
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/edit/:id', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
